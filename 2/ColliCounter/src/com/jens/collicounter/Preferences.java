f23899139c326e38873fb7172c52f005c920967b
omgjens <jlangen>
2013-08-16 23:18:49 +0200

    new file:   2/ColliCounter/AndroidManifest.xml 	new file:   2/ColliCounter/ic_launcher-web.png 	new file:   2/ColliCounter/libs/android-support-v4.jar 	new file:   2/ColliCounter/lint.xml 	new file:   2/ColliCounter/proguard-project.txt 	new file:   2/ColliCounter/project.properties 	new file:   2/ColliCounter/res/drawable-hdpi/action_settings.png 	new file:   2/ColliCounter/res/drawable-hdpi/ic_launcher.png 	new file:   2/ColliCounter/res/drawable-hdpi/navigation_refresh.png 	new file:   2/ColliCounter/res/drawable-mdpi/action_settings.png 	new file:   2/ColliCounter/res/drawable-mdpi/ic_launcher.png 	new file:   2/ColliCounter/res/drawable-mdpi/navigation_refresh.png 	new file:   2/ColliCounter/res/drawable-xhdpi/action_settings.png 	new file:   2/ColliCounter/res/drawable-xhdpi/ic_launcher.png 	new file:   2/ColliCounter/res/drawable-xhdpi/navigation_refresh.png 	new file:   2/ColliCounter/res/drawable-xxhdpi/ic_launcher.png 	new file:   2/ColliCounter/res/layout/activity_main.xml 	new file:   2/ColliCounter/res/menu-hdpi/mainmenu.xml 	new file:   2/ColliCounter/res/menu/main.xml 	new file:   2/ColliCounter/res/values-sw600dp/dimens.xml 	new file:   2/ColliCounter/res/values-sw720dp-land/dimens.xml 	new file:   2/ColliCounter/res/values-v11/styles.xml 	new file:   2/ColliCounter/res/values-v14/styles.xml 	new file:   2/ColliCounter/res/values/dimens.xml 	new file:   2/ColliCounter/res/values/strings.xml 	new file:   2/ColliCounter/res/values/styles.xml 	new file:   2/ColliCounter/res/xml/preferences.xml 	new file:   2/ColliCounter/src/com/jens/collicounter/MainActivity.java 	new file:   2/ColliCounter/src/com/jens/collicounter/Preferences.java 	deleted:    ColliCounter

---
+++ 2/ColliCounter/src/com/jens/collicounter/Preferences.java
@@ -0,0 +1,16 @@
+package com.jens.collicounter;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+
+}
